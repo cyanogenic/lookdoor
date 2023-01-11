78b8576be44779c7b2820d7ebad26aadf54b610f
cyanogenic <zhouziqi@ityut.com>
2023-01-11 09:01:14 +0800

    使用命令行参数传递door_number

--- lookdoor.py
+++ lookdoor.py
@@ -1,14 +1,12 @@
-import json
-import requests
 import base64
-import urllib.parse
+import configparser
+import json
 import re
+import requests
+import sys
+import urllib.parse
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
-
-PHONE_NUMBER = '在引号中填入手机号'
-PASSWORD = '在引号中填入密码'
-
 
 def MD5(msg):                   #将密码编码
     import hashlib
@@ -24,7 +22,7 @@ def encrypt(key, msg):          #模拟守望领域APP的方式进行AES加密
     return base64.b64encode(ct)
 
 
-def login():                    
+def login(phone_number, password):                    
     key_resp = requests.post('https://api.lookdoor.cn:443/func/hjapp/user/v2/getPasswordAesKey.json?') #获取cookie, 获取AES密钥
     global cookie
     cookie = key_resp.headers['set-cookie']
@@ -33,27 +31,32 @@ def login():
     cookie = match.group(1)
     aes_key = key_resp.json()['data']['aesKey']
     
-    password_md5 = MD5(PASSWORD)
+    password_md5 = MD5(password)
     password_encypted = urllib.parse.quote_plus(encrypt(aes_key, password_md5))             #加密密码
-    login_url = f'https://api.lookdoor.cn:443/func/hjapp/user/v2/login.json?password={password_encypted}&deviceId=&loginNumber={PHONE_NUMBER}&equipmentFlag=1'
+    login_url = f'https://api.lookdoor.cn:443/func/hjapp/user/v2/login.json?password={password_encypted}&deviceId=&loginNumber={phone_number}&equipmentFlag=1'
     requests.post(login_url, headers={'cookie': cookie})
     
     doorquery_url = f'https://api.lookdoor.cn/func/hjapp/house/v1/getEquipAccessListNew.json'         #查询门的ID
     global doorquery_resp
     doorquery_resp = requests.post(doorquery_url, headers={'cookie': cookie})
+    print(doorquery_resp.json())
 
 
 def unlock(door_number): 
     equipment_id = doorquery_resp.json()['data'][door_number]['id'] 
     unlock_url = f'https://api.lookdoor.cn:443/func/hjapp/house/v1/pushOpenDoorBySn.json?equipmentId={equipment_id}'       #发送开门请求
     unlock_resp = requests.post(unlock_url, headers={'cookie': cookie})
-    # print(unlock_resp.json()) 
+    print(unlock_resp.json())
     return unlock_resp.json()
 
 
 def main():
-    login()
-    unlock(0) # 若账号绑定多个门，则需要修改这里的door_number来指定开哪个门。
+    # 读取配置文件
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    login(config['lookdoor']['phone_number'], config['lookdoor']['password'])
+    unlock(int(sys.argv[1])) # 若账号绑定多个门，则需要修改这里的door_number来指定开哪个门。
     #unlock(1) # 也可以同时打开多个门。
 
 if __name__ == "__main__":
